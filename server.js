a8ae34c59f2fd6779d64c21a3bcdaa0b3e499844
gautamtrivedi49 <42693580+gautamtrivedi49@users.noreply.github.com>
2019-08-04 19:38:22 +0530

    Startups page

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const path = require("path");
-const {
-    MongoClient
-} = require("mongodb");
+const { MongoClient } = require("mongodb");
 require("dotenv").config();
 
-const startupController = require("./startup")
+const startupController = require("./startup");
 
-MongoClient.connect(process.env.MONGODB_URI, function (err, client) {
+MongoClient.connect(
+  process.env.MONGODB_URI,
+  {
+    useNewUrlParser: true
+  },
+  function(err, client) {
     if (err) {
-        return console.error(err)
+      return console.error(err);
     }
 
     const db = client.db();
@@ -19,10 +22,16 @@ MongoClient.connect(process.env.MONGODB_URI, function (err, client) {
     app.set("view engine", "ejs");
     app.set("views", path.join(__dirname, "views"));
 
-    app.get("/startups/:id", startupController.renderStartup(db))
+    app.get("/startups/:id", startupController.renderStartup(db));
+    app.get("/startups", startupController.showAllStartups(db));
+
+    app.get("/siicteam", function(req, res) {
+      res.render("siicteam", {});
+    });
 
     const port = process.env.PORT;
-    app.listen(port, function () {
-        console.log("Server started on port", port);
+    app.listen(port, function() {
+      console.log("Server started on port", port);
     });
-})
+  }
+);
--- startup.js
+++ startup.js
@@ -1,19 +1,41 @@
-module.exports.renderStartup = function (db) {
-    return function (req, res) {
-        const id = req.params.id;
-        db.collection("startups").findOne({
-            id: id
-        }).then(startup => {
-            if (startup == null) {
-                return res.status(404).send("not found")
-            }
+module.exports.renderStartup = function(db) {
+  return function(req, res) {
+    const id = req.params.id;
+    db.collection("startups")
+      .findOne({
+        id: id
+      })
+      .then(startup => {
+        if (startup == null) {
+          return res.status(404).send("not found");
+        }
 
-            res.render("startup", {
-                startup: startup
-            })
-        }).catch(err => {
-            console.error(err)
-            res.status(500).send("Error")
-        })
-    }
-}
+        res.render("startup", {
+          startup: startup
+        });
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(500).send("Error");
+      });
+  };
+};
+
+module.exports.showAllStartups = function(db) {
+  return function(req, res) {
+    db.collection("startups")
+      .find({}, { projection: { _id: 0, about: 0, achievements: 0 } })
+      .toArray()
+      .then(
+        startups => {
+          res.render("startups", {
+            startups: startups
+          });
+        },
+        err => {
+          console.error(err);
+          res.status(500).send("error");
+        }
+      );
+  };
+};
